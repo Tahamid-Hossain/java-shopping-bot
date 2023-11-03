465a704a53f57df767978c690a96ac6a8596ae5f
Tahamid Hossain <mitahamid13@gmail.com>
2023-11-04 00:33:46 +0400

    commit to open amazon

--- java_shopping_bot/src/main/java/com/shopping_bot/Main.java
+++ java_shopping_bot/src/main/java/com/shopping_bot/Main.java
@@ -5,10 +5,13 @@ import com.microsoft.playwright.*;
 public class Main {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
+            BrowserType browserType = playwright.chromium();
+            Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+
             Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            System.out.println(page.title());
-        }
+            page.navigate("https://amazon.ae");
+            page.locator("Search Amazon.ae");
+            page.click("EN");
+    }
     }
 }
