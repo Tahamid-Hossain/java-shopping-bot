d2dc4d28176f1129dff762e10fcc0a0846bdfc47
Tahamid Hossain <mitahamid13@gmail.com>
2023-11-04 18:44:03 +0400

    new code for search function

--- java_shopping_bot/src/main/java/com/shopping_bot/Main.java
+++ java_shopping_bot/src/main/java/com/shopping_bot/Main.java
@@ -4,16 +4,16 @@ import com.microsoft.playwright.*;
 
 public class Main {
     public static void main(String[] args) {
-        try (Playwright playwright = Playwright.create()) {
-            BrowserType browserType = playwright.chromium();
-            Browser browser = browserType.launch(
-                new BrowserType.LaunchOptions().setHeadless(false));
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(
+            new BrowserType.LaunchOptions().setHeadless(false)
+        );
+        Page page = browser.newPage();
+        page.navigate("https://amazon.ae");
+        page.getByPlaceholder("Search Amazon.ae").type("iPhone 15 pro max");
+        page.locator("id=nav-search-submit-button").click();
 
-            Page page = browser.newPage();
-            page.navigate("https://amazon.ae");
-            page.getByPlaceholder("Search amazon.ae").type("speakers");
-            page.locator("//input[@vlaue='Go']").click();
-            
-    }
+        
+
     }
 }
