ad41b56fb52b7f1b44aece12a39a6045ec88574b
Tahamid Hossain <mitahamid13@gmail.com>
2023-11-08 21:41:03 +0400

    newly added: user input for the product name

--- java_shopping_bot/src/main/java/com/shopping_bot/Main.java
+++ java_shopping_bot/src/main/java/com/shopping_bot/Main.java
@@ -1,15 +1,24 @@
 package com.shopping_bot;
 
+import java.util.Scanner;
 import com.microsoft.playwright.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter product name: ");
+        String productName = userInput.nextLine();
+
+        //System.out.println("Number of results to be shown: ");
+        //int nproducts = userInput.nextInt();
+
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(
             new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
         page.navigate("https://amazon.ae");
-        page.getByPlaceholder("Search Amazon.ae").type("iphone 15 pro max ");
+        page.getByPlaceholder("Search Amazon.ae").type(productName);
         page.locator("id=nav-search-submit-button").click();
         // page.locator("(//div[@role='button'])[1]").click();
         // page.locator("(//img[@class='s-image'])[1]").click();
